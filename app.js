e2f85cdd8a398598aeffbf297c9a39b8c1f3d788
Ahsan ali <120973691+AhsanAlijs@users.noreply.github.com>
2024-01-21 00:15:27 +0500

    Create Propper

--- app.js
+++ app.js
@@ -5,20 +5,38 @@ const phone = document.querySelector('#phone');
 const subject = document.querySelector('#subject');
 const message = document.querySelector('#message');
 
-function sendEmail(event) {
-    event.preventDefault();
-    let body = 'Name :' + names.value + "<br/> Email :" + email.value + "<br/> Phone :" + phone.value + "<br/> Subject :" + subject.value + "<br/> Message :" + message.value;
-    
+// function sendEmail(event) {
+//     event.preventDefault();
+//     let body = 'Name :' + names.value + "<br/> Email :" + email.value + "<br/> Phone :" + phone.value + "<br/> Subject :" + subject.value + "<br/> Message :" + message.value;
 
+//     Email.send({
+//         Host: "smtp.elasticemail.com",
+//         Username: "ahsanali45444@gmail.com",
+//         Password: "82626F54813F9DB1A8A08E76FFB06058BA31",
+//         To: 'fida35636@gmail.com',
+//         From: "ahsanali45444@gmail.com",
+//         Subject: "This is the subject",
+//         Body: "And this is the body"
+//     }).then(
+//         message => alert(message)
+//     );
 
-    Email.send({
-        SecureToken: '58c55465-d41e-42b4-9d76-d282b83e01f4',
-        To: `ahsanali45444@gmail.com`,
-        From: `ahsanali45444@gmail.com`,
-        Subject: `message from contact`,
-        Body: body
-    }).then(
-        message => alert(message)
-    );
+//
 
-}
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log('Function Called');
+
+})
+
+Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "ahsanali45444@gmail.com",
+    Password: "82626F54813F9DB1A8A08E76FFB06058BA31",
+    To: 'ahsanali45444@gmail.com',
+    From: "ahsanali45444@gmail.com",
+    Subject: "This is the subject",
+    Body: "And this is the body"
+}).then(
+    message => alert(message)
+);
